c4f0a075913e675b820d39eee672717944a03250
Dimitri Sokolyuk <demon@dim13.org>
2018-01-11 00:18:56 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/dim13/file2go.svg?branch=master)](https://travis-ci.org/dim13/file2go)
+
+# file2go
+
+Dead-simple file embedding tool for Go
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	golden, err := ioutil.ReadFile("testdata/empty_gz.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	bin, err := os.Open("testdata/empty.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	err = generate(buf, bin, "empty.gz", "testdata", "file2go -in empty.gz -pkg testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(buf.Bytes(), golden) {
+		t.Error("not equal")
+	}
+}
